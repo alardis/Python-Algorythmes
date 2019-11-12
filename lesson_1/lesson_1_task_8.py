d18dcd7b9f22435c884af3c9784de04df5083937
VIZEX\ashumov <ashumov@vizex.ru>
2019-11-12 14:09:44 +0300

    Восьмое задание сделано

--- lesson_1/lesson_1_task_8.py
+++ lesson_1/lesson_1_task_8.py
@@ -1,3 +1,27 @@
 # Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
 
 
+a = int(input('Введите первое число:\n'))
+b = int(input('Введите второе число:\n'))
+c = int(input('Введите третье число:\n'))
+
+mean_num = None
+
+if a > b:
+    if a > c:
+        if b > c:
+            mean_num = b
+        else:
+            mean_num = c
+    else:
+        mean_num = a
+else:
+    if b > c:
+        if a > c:
+            mean_num = a
+        else:
+            mean_num = c
+    else:
+        mean_num = b
+
+print(f'Среднее число из введенных -- {mean_num}')
