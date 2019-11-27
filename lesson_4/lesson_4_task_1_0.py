a16646d35a19d44b0e81ae8510cc845bd49d7e7c
alardis <novacat.a@gmail.com>
2019-11-27 21:41:11 +0300

    Прогресс по первому заданию

--- lesson_4/lesson_4_task_1_0.py
+++ lesson_4/lesson_4_task_1_0.py
@@ -36,7 +36,20 @@ def get_max_from_min(size_x, size_y):
     # return max_min
 
 
-cProfile.run('get_max_from_min(5, 5)')
+def get_max_from_min_1(size_x, size_y):
+    matrix = [[random.randint(0, 20) for _ in range(size_x)] for _ in range(size_y)]
+    min_list = []
+
+    for i in range(len(matrix[0])):
+        col_list = [matrix[j][i] for j in range(len(matrix))]
+        min_item = min(col_list)
+        min_list.append(min_item)
+
+    max_min = max(min_list)
+    # return max_min
+
+
+# cProfile.run('get_max_from_min(5, 5)')
 # result = get_max_from_min(400, 200)
 
 # (venv) D:\YandexDisk\РАБОТА\Python\Python-Algorythmes\lesson_4>
@@ -49,4 +62,6 @@ cProfile.run('get_max_from_min(5, 5)')
 # 100 loops, best of 5: 44.1 msec per loop   ---- 200, 200
 # 100 loops, best of 5: 90.4 msec per loop   ---- 400, 200
 
+
+# GET_MAX_FROM_MIN_1()
 
