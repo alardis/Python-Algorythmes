ffb87c9f6c075b4afe73468f421ec85a3afbec02
VIZEX\ashumov <ashumov@vizex.ru>
2019-11-29 09:54:48 +0300

    Второе задание доделано

--- lesson_4/lesson_4_task_2_0.py
+++ lesson_4/lesson_4_task_2_0.py
@@ -1,20 +1,53 @@
 # Написать два алгоритма нахождения i-го по счёту простого числа. Функция нахождения простого числа должна принимать
 # на вход натуральное и возвращать соответствующее простое число. Проанализировать скорость и сложность алгоритмов.
 # Первый — с помощью алгоритма «Решето Эратосфена».
+# Второй — с помощью классического алгоритма.
 
-n = int(input('До какого числа получить простые числа:  '))
 
-def get_simple_num(count):
-    sieve_list = []
-    number = 2
+# n = int(input('До какого числа получить простые числа:  '))
+
+
+def sieve(count):
+    sieve_list = [2, ]
+    number = 3
 
     # взять цикл while, продлевать цифровой ряд каждый раз, пока оно надо
+    while len(sieve_list) < count:
+        add_flag = True
+        for item in sieve_list:
+            if number % item == 0:
+                add_flag = False
+                break
+        if add_flag:
+            sieve_list.append(number)
+        number += 1
 
-    for i in range(2, n):
-        if sieve[i] != 0:
-            j = i * 2
-            while j < n:
-                sieve[j] = 0
-                j += i
+    # print(sieve_list)
+    # print(f'Нашли простое {count} число -- {sieve_list[count-1]}')
 
-    result = [i for i in sieve if i != 0]
+
+def prime(count):
+    prime_list = [2, ]
+
+    number = 3
+    while len(prime_list) < count:
+        d = 2
+        while number % d != 0:
+            d += 1
+        if number == d:
+            prime_list.append(number)
+        number += 1
+
+    # print(prime_list)
+    # print(f'Нашли простое {count} число -- {prime_list[count - 1]}')
+
+
+# sieve(n)
+# 100 loops, best of 5: 862 usec per loop  ---  100
+# 100 loops, best of 5: 83.5 msec per loop ---  1000
+
+# prime(n)
+# 100 loops, best of 5: 4.26 msec per loop ---  100
+# 100 loops, best of 5: 812 msec per loop ---  1000
+
+# ВЫВОД: однозначно Эратосфен отлично придумал.
