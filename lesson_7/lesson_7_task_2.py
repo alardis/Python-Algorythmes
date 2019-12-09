18ee5bb96dae981fe0e6e10ae7a4fd2f971fc19b
alardis <novacat.a@gmail.com>
2019-12-09 21:56:13 +0300

    Второе задание выполнено

--- lesson_7/lesson_7_task_2.py
+++ lesson_7/lesson_7_task_2.py
@@ -6,7 +6,44 @@ import random
 
 
 size = 20
-my_list = [random.uniform(0.0, 50.0) for _ in range(size)]
-print(f'Исходный массив: {my_list}')
+unsorted_list = [random.uniform(0.0, 50.0) for _ in range(size)]
+print(f'Исходный массив: {unsorted_list}')
 
+
+def merge_sort(my_list):
+    print("Splitting ", my_list)
+    if len(my_list) > 1:
+        mid = len(my_list) // 2
+        left_half = my_list[:mid]
+        right_half = my_list[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                my_list[k] = left_half[i]
+                i += 1
+            else:
+                my_list[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            my_list[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            my_list[k] = right_half[j]
+            j += 1
+            k += 1
+    # print("Merging ", my_list)
+    return my_list
+
+
+print(f'Отсортированный массив: {merge_sort(unsorted_list)}')
 
