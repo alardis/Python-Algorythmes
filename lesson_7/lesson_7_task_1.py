40e70d465c9d505e98e425ae9f17e99394c08ee0
VIZEX\ashumov <ashumov@vizex.ru>
2019-12-09 12:49:45 +0300

    Первое задание сделано

--- lesson_7/lesson_7_task_1.py
+++ lesson_7/lesson_7_task_1.py
@@ -10,5 +10,37 @@ import random
 
 
 size = 10
-my_list = [random.randint(-size, size) for _ in range(size)]
-print(f'Исходный массив: {my_list}')
+unsorted_list = [random.randint(-size, size) for _ in range(size)]
+print(f'Исходный массив: {unsorted_list}')
+
+
+# реализуем алгоритм сортировки пузырьком
+def sort_bubble(my_list):
+    n = 1
+    while n < len(my_list):
+        for i in range(len(my_list) - n):
+            if my_list[i] > my_list[i + 1]:
+                my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+        n += 1
+    return my_list
+
+
+# реализуем алгоритм сортировки пузырьком с улучшениями
+# алгоритм запоминает, был ли какой-то обмен при текущем обходе. если обмена не было, цикл вновь не стартует. значит,
+# все и так уже отсортировано. вполне себе улучшение.
+def sort_bubble_2(my_list):
+    n = 1
+    swap = True
+    while n < len(my_list) and swap:
+        swap = False
+        for i in range(len(my_list) - n):
+            if my_list[i] > my_list[i + 1]:
+                my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+                swap = True
+        n += 1
+    return my_list
+
+
+# работа с функцией
+print(f'Отсортированный массив: {sort_bubble_2(unsorted_list)}')
+
