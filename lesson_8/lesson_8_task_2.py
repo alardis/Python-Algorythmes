d53de6153b572ce9e1833898d5e80a24f55db840
alardis <novacat.a@gmail.com>
2019-12-14 22:13:09 +0300

    Второе задание сделано

--- lesson_8/lesson_8_task_2.py
+++ lesson_8/lesson_8_task_2.py
@@ -38,20 +38,24 @@ def deykstra_ench(my_graph, start):
     route_dict = {}
     route_dict.update({start: 'start'})
 
+    route_list = []
     while min_cost < float('inf'):
         is_visited[start] = True
+        route_list.append(start)
 
-        route_list = []
         for i, vertex in enumerate(my_graph[start]):
             if vertex != 0 and not is_visited[i]:
                 print(f'Рассматриваем вершину {i}, расстояние до нее -- {vertex}')
-                route_list.append(i)
                 if cost[i] > vertex + cost[start]:
-                    print(f'Прошлое расстояние {cost[i]}, нынешнее -- {vertex + cost[start]}')
+                    print(f'Вершина {i}. Прошлое расстояние {cost[i]}, нынешнее -- {vertex + cost[start]}')
                     print(f'route_list: {route_list}')
-                    route_dict.update({i: route_list})
+                    if i not in route_dict.keys():
+                        route_dict.update({i: route_list[:]})
+                    print(f'route_dict: {route_dict}')
+                    route_list.append(i)
                     cost[i] = vertex + cost[start]
                     parent[i] = start
+                    # print(f'parent: {parent}')
 
         min_cost = float('inf')
         for i in range(length):
@@ -63,12 +67,12 @@ def deykstra_ench(my_graph, start):
 
 
 point = int(input('Из какой вершины идти:  '))
-n = random.randint(2, 20)
+n = random.randint(2, 10)
 print('\n' + '*' * 50)
 graph = get_graph(n)
 routes = deykstra_ench(graph, point)
 print(routes)
 
 # визуализация графа
-g_vis = nx.from_dict_of_lists(graph)
-nx.draw(g_vis)
+# g_vis = nx.from_dict_of_lists(graph)
+# nx.draw(g_vis)
